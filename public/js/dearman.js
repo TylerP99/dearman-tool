e100b0f4bce42d7a2b1845f2a45b0d5d6c01b589
Tyler Perata <tylerperata@gmail.com>
2022-08-28 22:02:09 -0700

    Basic form create functionality

--- public/js/dearman.js
+++ public/js/dearman.js
@@ -1,17 +1,21 @@
+
+const generateButton = document.querySelector("#submit-button");
+generateButton.addEventListener("click", create_dearman_guide);
+
+function create_dearman_guide() {
+    const targetSection = document.querySelector(".dearman-guide-result");
+    const dearmanMaker = new Dearman_entry;
+    dearmanMaker.append_document(targetSection);
+}
+
+
+
+
+
 // Defined dearman entry object
 // Gets information from form set
 
 class Dearman_entry {
-    constructor() {
-        this.topic;
-        this.describe_entry;
-        this.express_entry;
-        this.assert_entry;
-        this.reinforce_entry;
-        this.mindful_entry;
-        this.appear_confident_entry;
-        this.negotiate_entry;
-    }
 
     get_topic() {
         const topicForm = document.querySelector(".topic-form");
@@ -40,28 +44,127 @@ class Dearman_entry {
     get_reinforcement_entry() {
         // Can be multiple or just one
         const reinforcmentForm = document.querySelector(".reinforcement-form");
-        const reinforcements = reinforcmentForm.querySelectorAll(".reinforcement");
-        reinforcements = reinforcements.map(x => x.value);
+        let reinforcements = reinforcmentForm.querySelectorAll(".reinforcement");
+        reinforcements = Array.from(reinforcements).map(x => x.value);
         return reinforcements;
     }
 
     get_mindful_entry() {
         const mindfulForm = document.querySelector(".mindful-form");
-        const strategies = mindfulForm.querySelectorAll(".strategy");
-        strategies = strategies.map(x=>x.value);
+        let strategies = mindfulForm.querySelectorAll(".strategy");
+        strategies = Array.from(strategies).map(x=>x.value);
         return strategies;
     }
 
     get_appear_confident_entry() {
         const confidentForm = document.querySelector(".appear-confident-form");
-        const formEntries = confidentForm.querySelectorAll("appear-confident");
-        formEntries = formEntries.map(x=>x.value);
+        let formEntries = confidentForm.querySelectorAll("appear-confident");
+        formEntries = Array.from(formEntries).map(x=>x.value);
         return formEntries;
     }
 
     get_negotiate_entry() {
         const negotiateForm = document.querySelector(".negotiate-form");
-        const negotiations = negotiateForm.querySelectorAll("negotiation");
-        negotiations = negotiations.map(x=>x.value);
+        let negotiations = negotiateForm.querySelectorAll("negotiation");
+        negotiations = Array.from(negotiations).map(x=>x.value);
+        return negotiations;
+    }
+
+    generate_page() {
+        const container = document.createElement("article");
+
+        const heading = document.createElement("h2");
+        heading.innerText = "DEAR MAN Reference";
+        container.appendChild(heading);
+
+        const describeSection = document.createElement("section");
+        const describeHeader = document.createElement("h4");
+        describeHeader.innerText = "Describe";
+        const description = document.createElement("p");
+        description.innerText = this.get_describe_entry();
+        describeSection.appendChild(describeHeader);
+        describeSection.appendChild(description);
+        container.appendChild(describeSection);
+
+        const expressSection = document.createElement("section");
+        const expressHeader = document.createElement("h4");
+        expressHeader.innerText = "Express";
+        const expression = document.createElement("p");
+        expression.innerText = this.get_express_entry();
+        expressSection.appendChild(expressHeader);
+        expressSection.appendChild(expression);
+        container.appendChild(expressSection);
+
+        const assertSection = document.createElement("section");
+        const assertHeader = document.createElement("h4");
+        assertHeader.innerText = "Assert";
+        const assertion = document.createElement("p");
+        assertion.innerText = this.get_assert_entry();
+        assertSection.appendChild(assertHeader);
+        assertSection.appendChild(assertion);
+        container.appendChild(assertSection);
+
+        const reinforceSection = document.createElement("section");
+        const reinforceHeader = document.createElement("h4");
+        reinforceHeader.innerText = "Reinforce";
+        const reinforcementList = document.createElement("ul");
+        const reinforcements = this.get_reinforcement_entry();
+        reinforcements.forEach(x=>{
+            const listItem = document.createElement("li");
+            listItem.innerText = x;
+            reinforcementList.appendChild(listItem);
+        });
+        reinforceSection.appendChild(reinforceHeader);
+        reinforceSection.appendChild(reinforcementList);
+        container.appendChild(reinforceSection);
+
+        const mindfulSection = document.createElement("section");
+        const mindfulHeader = document.createElement("h4");
+        mindfulHeader.innerText = "Stay Mindful";
+        const mindfulList = document.createElement("ul");
+        const strategies = this.get_mindful_entry();
+        strategies.forEach(x => {
+            const li = document.createElement("li");
+            li.innerText = x;
+            mindfulList.appendChild(li);
+        });
+        mindfulSection.appendChild(mindfulHeader);
+        mindfulSection.appendChild(mindfulList);
+        container.appendChild(mindfulSection);
+
+        const appearConfidentSection = document.createElement("section");
+        const appearConfidentHeader = document.createElement("h4");
+        appearConfidentHeader.innerText = "Appear Confident";
+        const appearConfidentList = document.createElement("ul");
+        const confidentReminders = this.get_appear_confident_entry();
+        confidentReminders.forEach( x => {
+            const li = document.createElement("li");
+            li.innerText = x;
+            appearConfidentList.appendChild(li);
+        });
+        appearConfidentSection.appendChild(appearConfidentHeader);
+        appearConfidentSection.appendChild(appearConfidentList);
+        container.appendChild(appearConfidentSection);
+
+        const negotiateSection = document.createElement("section");
+        const negotiateHeader = document.createElement("h4");
+        negotiateHeader.innerText = "Negotiate";
+        const negotiationList = document.createElement("ul");
+        const negotiations = this.get_negotiate_entry();
+        negotiations.forEach( x => {
+            const li = document.createElement("li");
+            li.innerText = x;
+            negotiationList.appendChild(li);
+        });
+        negotiateSection.appendChild(negotiateHeader);
+        negotiateSection.appendChild(negotiationList);
+        container.appendChild(negotiateSection);
+
+        return container;
+    }
+
+    append_document(target) {
+        const form = this.generate_page();
+        target.appendChild(form);
     }
 }
